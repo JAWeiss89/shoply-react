c3926f2add8e59d987617ee8dc207d891a1802f2
Jorge Weiss <jorge.a.weiss@gmail.com>
2020-11-30 19:26:10 -0600

    Create directories for styles and components. Add redux and react-redux to dependencies.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Store from './Store.js';
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Store />
+    </div>
+  );
+};
+
+export default App;
+
